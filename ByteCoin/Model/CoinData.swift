39aa0588307c70ab4241a8fed00dfe053358be6a
Nathan Mora <nathanmorax@outlook.com>
2022-05-05 23:22:31 -0500

    version 1

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Nathan Mora on 05/05/22.
+//  Copyright © 2022 Nathan Mora. All rights reserved.
+//
+
+import Foundation
+
+struct CoindData: Decodable {
+   let rate: Double
+}
